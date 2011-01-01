b4b30c42f8481cd6b88004f7b2870a4525c6172f
Petr Gladkikh <PetrGlad@gmail.com>
2011-01-01 22:21:22 +0600

    Cleanup.

--- src/petrglad/javarpc/client/Client.java
+++ src/petrglad/javarpc/client/Client.java
@@ -11,91 +11,88 @@ import com.google.common.collect.Sets;
 
 public class Client {
 
-	static final Logger LOG = Logger.getLogger(Client.class);
+    static final Logger LOG = Logger.getLogger(Client.class);
 
-	final private ClientSession session;
+    final private ClientSession session;
 
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.out.println("Usage: Client host port");
-		} else {
-			LOG.info("Started " + Arrays.toString(args));
-			final Client client = new Client(args[0], Integer.parseInt(args[1]));
-			LOG.info("Response " + client.call("calculator.add", 3L, 4L));
-			concurrentTest(client);
-			LOG.info("Finished.");
-		}
-	}
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: Client host port");
+        } else {
+            LOG.info("Started " + Arrays.toString(args));
+            final Client client = new Client(args[0], Integer.parseInt(args[1]));
+            LOG.info("Response " + client.call("calculator.add", 3L, 4L));
+            concurrentTest(client);
+            LOG.info("Finished.");
+        }
+    }
 
-	private static void concurrentTest(final Client client) {
-		Set<Thread> threads = Sets.newHashSet();
-		for (int ti = 0; ti < 10; ti++) {
-			final long threadNo = ti;
-			Thread t = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					int N = 10000;
-					final Set<Long> tokens = Sets
-							.newHashSetWithExpectedSize(N);
-					for (long i = 0; i < N; i++)
-						tokens.add(client.send("calculator.guess",
-								threadNo, i));
-					while (!tokens.isEmpty()) {
-						Long id = tokens.iterator().next();
-						Object result = client.receive(id);
-						if (null != result) {
-							LOG.info("Request id=" + id + ", result="
-									+ result);
-							tokens.remove(id);
-						}
-					}
-				}
-			});
-			threads.add(t);
-			t.setName("Client " + ti);
-			t.start();
-		}
-		for (Thread t : threads) {
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-			}
-		}
-	}
+    private static void concurrentTest(final Client client) {
+        Set<Thread> threads = Sets.newHashSet();
+        for (int ti = 0; ti < 10; ti++) {
+            final long threadNo = ti;
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int N = 10000;
+                    final Set<Long> tokens = Sets.newHashSetWithExpectedSize(N);
+                    for (long i = 0; i < N; i++)
+                        tokens.add(client.send("calculator.add", threadNo, i));
+                    while (!tokens.isEmpty()) {
+                        Long id = tokens.iterator().next();
+                        Object result = client.receive(id);
+                        if (null != result) {
+                            LOG.info("Request id=" + id + ", result=" + result);
+                            tokens.remove(id);
+                        }
+                    }
+                }
+            });
+            threads.add(t);
+            t.setName("Client " + ti);
+            t.start();
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 
-	public Client(String host, int port) {
-		this.session = new ClientSession(new ServerProxy(host, port));
-	}
+    public Client(String host, int port) {
+        this.session = new ClientSession(new ServerProxy(host, port));
+    }
 
-	public Object call(String methodName, Object... params) {
-		long messageId = send(methodName, params);
-		// XXX Better to wait on condition instead of just polling. Asynchronous
-		// call interface would be more natural choice here.
-		while (true) {
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-			}
-			Object result = receive(messageId);
-			if (null != result)
-				return result;
-		}
-	}
+    public Object call(String methodName, Object... params) {
+        long messageId = send(methodName, params);
+        // XXX Better to wait on condition instead of just polling. Asynchronous
+        // call interface would be more natural choice here.
+        while (true) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+            }
+            Object result = receive(messageId);
+            if (null != result)
+                return result;
+        }
+    }
 
-	private Object receive(long messageId) {
-		Response response = session.getResponse(messageId);
-		if (null != response) {
-			RuntimeException e = response.asException();
-			if (null != e) {
-				throw e;
-			} else {
-				return response.value;
-			}
-		} else
-			return null;
-	}
+    private Object receive(long messageId) {
+        Response response = session.getResponse(messageId);
+        if (null != response) {
+            RuntimeException e = response.asException();
+            if (null != e) {
+                throw e;
+            } else {
+                return response.value;
+            }
+        } else
+            return null;
+    }
 
-	private long send(String methodName, Object... params) {
-		return session.send(methodName, Arrays.asList(params));
-	}
+    private long send(String methodName, Object... params) {
+        return session.send(methodName, Arrays.asList(params));
+    }
 }
--- src/petrglad/javarpc/client/ServerProxy.java
+++ src/petrglad/javarpc/client/ServerProxy.java
@@ -1,10 +1,8 @@
 package petrglad.javarpc.client;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -13,81 +11,78 @@ import petrglad.javarpc.Response;
 
 /**
  * Handles connection to server and message serialization.
+ * 
  * @author petr
  */
 public class ServerProxy {
 
-	private final String host;
-	private final int port;
+    private final String host;
+    private final int port;
 
-	private Socket socket = null;
-	private ObjectOutputStream oOut = null;
-	private ObjectInputStream oIn = null;
+    private Socket socket = null;
+    private ObjectOutputStream oOut = null;
+    private ObjectInputStream oIn = null;
 
-	public ServerProxy(String host, int port) {
-		this.host = host;
-		this.port = port;
-	}
+    public ServerProxy(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
 
-	public void open() {
-		assert null == socket;
-		assert null == oOut;
-		assert null == oIn;
-		try {
-			socket = new Socket(host, port);
+    public void open() {
+        assert null == socket;
+        assert null == oOut;
+        assert null == oIn;
+        try {
+            socket = new Socket(host, port);
+            oOut = new ObjectOutputStream(socket.getOutputStream());
+            oIn = new ObjectInputStream(socket.getInputStream());
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Host is not found " + host + ":" + port);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not open connection to " + host);
+        }
+    }
 
-			OutputStream out = socket.getOutputStream();
-			oOut = new ObjectOutputStream(out);
+    public void send(Message m) {
+        try {
+            synchronized (oOut) {
+                oOut.writeObject(m);
+                oOut.flush();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-			InputStream in = socket.getInputStream();
-			oIn = new ObjectInputStream(in);
-		} catch (UnknownHostException e) {
-			throw new RuntimeException("Host is not found " + host + ":" + port);
-		} catch (IOException e) {
-			throw new RuntimeException("Could not open connection to " + host);
-		}
-	}
+    public Response receive() {
+        try {
+            synchronized (oIn) {
+                final Object o = oIn.readObject();
+                if (o instanceof Response) {
+                    return (Response) o;
+                } else {
+                    throw new RuntimeException(
+                            "Response object is of unexpected type "
+                                    + o.getClass().getCanonicalName());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public void send(Message m) {
-		try {
-			synchronized (oOut) {
-				oOut.writeObject(m);
-				oOut.flush();
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public Response receive() {
-		try {
-			synchronized (oIn) {
-				Object o = oIn.readObject();
-				if (o instanceof Response) {
-					return (Response) o;
-				} else {
-					throw new RuntimeException(
-							"Response object is of unexpected type "
-									+ o.getClass().getCanonicalName());
-				}
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public void close() {
-		try {
-			oOut.close();
-		} catch (IOException e) {
-		}
-		try {
-			oIn.close();
-		} catch (IOException e) {
-		}
-		try {
-			socket.close();
-		} catch (IOException e) {
-		}
-	}
+    public void close() {
+        try {
+            oOut.close();
+        } catch (IOException e) {
+        }
+        try {
+            oIn.close();
+        } catch (IOException e) {
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+        }
+    }
 }
